04da4391ebbe66f4af1f4d04f90895fee9ab8fb0
Xiexuc <liachenxiexu@gmail.com>
2020-10-13 21:02:17 +1100

    set firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Title from './comps/Title';
+import Title from './component/Title';
+import UploadForm from './component/UploadForm';
 
 function App() {
   return (
     <div className="App">
       <Title/>
+      <UploadForm />
     </div>
   );
 }
---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDyoI70H0vfI9JCeTxu3zzk8TnHl69ktQo",
+    authDomain: "xiexuc-mygallery.firebaseapp.com",
+    databaseURL: "https://xiexuc-mygallery.firebaseio.com",
+    projectId: "xiexuc-mygallery",
+    storageBucket: "xiexuc-mygallery.appspot.com",
+    messagingSenderId: "399983928342",
+    appId: "1:399983928342:web:80684166cdba4692a24137"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
